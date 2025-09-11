3fe5524bb3a809964f27c30b7c4a7cc9c29e4b77
rlavlfwhd <rlavlwhd153@naver.com>
2025-09-11 11:47:36 +0900

    [Feat] VoxEdit 유니티 연동, 애니메이션 테스트

---
+++ Assets/01.Scripts/PlayerController.cs
@@ -0,0 +1,78 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [Header("이동 설정")]
+    public float walkSpeed = 3.0f;
+    public float runSpeed = 6.0f;
+    public float rotationSpeed = 10.0f;
+
+    [Header("공격 설정")]
+    public float attackDuration = 0.8f;
+    public bool canMoveWhileAttacking = false;
+
+    [Header("컴포넌트")]
+    public Animator animator;
+
+    private CharacterController controller;
+    private Camera playerCamera;
+
+    private float currentSpeed;
+    private bool isAttacking = false;
+
+    private void Start()
+    {
+        controller = GetComponent<CharacterController>();
+        playerCamera = Camera.main;
+    }
+
+    private void Update()
+    {
+        HandleMovement();
+        UpdateAinmator();
+    }
+
+    void HandleMovement()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        if(horizontal != 0 || vertical != 0)
+        {
+            Vector3 cameraForward = playerCamera.transform.forward;
+            Vector3 cameraRight = playerCamera.transform.right;
+            cameraForward.y = 0;
+            cameraRight.y = 0;
+            cameraForward.Normalize();
+            cameraRight.Normalize();
+
+            Vector3 moveDirection = cameraForward * vertical + cameraRight * horizontal;
+
+            if(Input.GetKey(KeyCode.LeftShift))
+            {
+                currentSpeed = runSpeed;
+            }
+            else
+            {
+                currentSpeed = walkSpeed;
+            }
+
+            controller.Move(moveDirection * currentSpeed * Time.deltaTime);
+
+            Quaternion targetRotation = Quaternion.LookRotation(moveDirection);
+            transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
+        }
+        else
+        {
+            currentSpeed = 0;
+        }
+    }
+
+    void UpdateAinmator()
+    {
+        float animatorSpeed = Mathf.Clamp01(currentSpeed / runSpeed);
+        animator.SetFloat("speed", animatorSpeed);
+    }
+}
